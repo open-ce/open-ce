db44e2bda662468eb515d9d3c50229a9d2ed500a
Ketan Kunde <ketank@us.ibm.com>
2020-10-08 16:26:17 +0530

    change to replace '.' and '_' with '-'

--- open-ce/build_tree.py
+++ open-ce/build_tree.py
@@ -69,7 +69,8 @@ class BuildCommand():
         """
         Returns a name representing the Build Command
         """
-        result = self.recipe
+        result = self.recipe.replace(".","-")
+        result = result.replace("_","-")
         if self.python:
             result +=  "-py" + self.python.replace(".","")
         if self.build_type:
