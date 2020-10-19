2d887bf2ff38ea29b1a317ffebd9a7328bf04f7c
Deepali Chourasia <deepch23@in.ibm.com>
2020-10-19 07:49:57 -0500

    fix whitespace

--- open-ce/docker_build.py
+++ open-ce/docker_build.py
@@ -32,7 +32,7 @@ def build_image():
     build_cmd += "-t " + image_name + " "
     build_cmd += "--build-arg BUILD_ID=" + str(os.getuid()) + " "
     build_cmd += "--build-arg GROUP_ID=" + str(os.getgid()) + " "
-    build_cmd += BUILD_IMAGE_PATH 
+    build_cmd += BUILD_IMAGE_PATH
 
     result = os.system(build_cmd)
 
