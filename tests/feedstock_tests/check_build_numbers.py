b59fe3f774f5d2100f312f3ffb8fccfe19c9b5eb
Bryant Nelson <bryant.nelson@ibm.com>
2020-10-21 18:19:55 -0500

    Update check_build_numbers.

--- tests/feedstock_tests/check_build_numbers.py
+++ tests/feedstock_tests/check_build_numbers.py
@@ -46,52 +46,56 @@ def main(arg_strings=None):
 
     config = get_or_merge_config(None)
     config.variant_config_files = [utils.DEFAULT_CONDA_BUILD_CONFIG]
-    recipe_conda_build_config = os.path.join(os.getcwd(), "config", "conda_build_config.yaml")
-    if os.path.exists(recipe_conda_build_config):
-        config.variant_config_files += [recipe_conda_build_config]
+
+    recipe_conda_build_config = build_feedstock.get_conda_build_config()
+    if recipe_conda_build_config:
+        config.variant_config_files.append(recipe_conda_build_config)
     config.verbose = False
+    variant_build_results = dict()
+    for variant in variants:
+        utils.run_and_log("git checkout {}".format(default_branch))
+        master_build_numbers = dict()
+        for recipe in build_config_data["recipes"]:
+            metas = conda_build.api.render(recipe['path'],
+                                        config=config,
+                                        variants=variant,
+                                        bypass_env_check=True,
+                                        finalize=False)
+            for meta,_,_ in metas:
+                master_build_numbers[meta.meta['package']['name']] = {"version" : meta.meta['package']['version'],
+                                                                    "number" : meta.meta['build']['number']}
 
-    utils.run_and_log("git checkout {}".format(default_branch))
-    master_build_numbers = dict()
-    for recipe in build_config_data["recipes"]:
-        metas = conda_build.api.render(recipe['path'],
-                                    config=config,
-                                    variants=variants[0],
-                                    bypass_env_check=True,
-                                    finalize=False)
-        for meta,_,_ in metas:
-            master_build_numbers[meta.meta['package']['name']] = {"version" : meta.meta['package']['version'],
-                                                                  "number" : meta.meta['build']['number']}
+        utils.run_and_log("git checkout {}".format(pr_branch))
+        current_pr_build_numbers = dict()
+        for recipe in build_config_data["recipes"]:
+            metas = conda_build.api.render(recipe['path'],
+                                        config=config,
+                                        variants=variant,
+                                        bypass_env_check=True,
+                                        finalize=False)
+            for meta,_,_ in metas:
+                current_pr_build_numbers[meta.meta['package']['name']] = {"version" : meta.meta['package']['version'],
+                                                                        "number" : meta.meta['build']['number']}
 
-    utils.run_and_log("git checkout {}".format(pr_branch))
-    current_pr_build_numbers = dict()
-    for recipe in build_config_data["recipes"]:
-        metas = conda_build.api.render(recipe['path'],
-                                    config=config,
-                                    variants=variants[0],
-                                    bypass_env_check=True,
-                                    finalize=False)
-        for meta,_,_ in metas:
-            current_pr_build_numbers[meta.meta['package']['name']] = {"version" : meta.meta['package']['version'],
-                                                                      "number" : meta.meta['build']['number']}
+        print("Build Info for Variant:   {}".format(variant))
+        print("Current PR Build Info:    {}".format(current_pr_build_numbers))
+        print("Master Branch Build Info: {}".format(master_build_numbers))
 
-    print("Current PR Build Info:    ", current_pr_build_numbers)
-    print("Master Branch Build Info: ", master_build_numbers)
+        #No build numbers can go backwards without a version change.
+        for package in master_build_numbers:
+            if package in current_pr_build_numbers and current_pr_build_numbers[package]["version"] == master_build_numbers[package]["version"]:
+                assert int(current_pr_build_numbers[package]["number"]) >= int(master_build_numbers[package]["number"]), "If the version doesn't change, the build number can't be reduced."
 
-    #No build numbers can go backwards without a version change.
-    for package in master_build_numbers:
-        if package in current_pr_build_numbers and current_pr_build_numbers[package]["version"] == master_build_numbers[package]["version"]:
-            assert int(current_pr_build_numbers[package]["number"]) >= int(master_build_numbers[package]["number"]), "If the version doesn't change, the build number can't be reduced."
+        #If packages are added or removed, don't require a version change
+        if set(master_build_numbers.keys()) != set(current_pr_build_numbers.keys()):
+            return
 
-    #If packages are added or removed, don't require a version change
-    if set(master_build_numbers.keys()) != set(current_pr_build_numbers.keys()):
-        return
-
-    #At least one package needs to increase the build number or change the version.
-    checks = [current_pr_build_numbers[package]["version"] != master_build_numbers[package]["version"] or
-              int(current_pr_build_numbers[package]["number"]) > int(master_build_numbers[package]["number"])
-                  for package in master_build_numbers]
-    assert any(checks), "At least one package needs to increase the build number or change the version."
+        #At least one package needs to increase the build number or change the version.
+        checks = [current_pr_build_numbers[package]["version"] != master_build_numbers[package]["version"] or
+                int(current_pr_build_numbers[package]["number"]) > int(master_build_numbers[package]["number"])
+                    for package in master_build_numbers]
+        variant_build_results[utils.variant_string(variant["python"], variant["build_type"], variant["mpi_type"])] = any(checks)
+    assert any(variant_build_results.values()), "At least one package needs to increase the build number or change the version in at least one variant."
 
 if __name__ == '__main__':
     try:
