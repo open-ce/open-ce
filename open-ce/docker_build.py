899d5ec28d08c1e3f7c18d3c5a64943a48ae824d
Deepali Chourasia <deepch23@in.ibm.com>
2021-02-03 07:29:04 -0600

    use buildtype in imagename

--- open-ce/docker_build.py
+++ open-ce/docker_build.py
@@ -47,12 +47,15 @@ def make_parser():
 
     return parser
 
-def build_image(build_image_path, dockerfile):
+def build_image(build_image_path, dockerfile, cuda_version=None):
     """
     Build a docker image from the Dockerfile in BUILD_IMAGE_PATH.
     Returns a result code and the name of the new image.
     """
-    image_name = REPO_NAME + ":" + IMAGE_NAME + "-" + str(os.getuid())
+    if cuda_version:
+        image_name = REPO_NAME + ":" + IMAGE_NAME + "-cuda" + cuda_version + "-" + str(os.getuid())
+    else:
+        image_name = REPO_NAME + ":" + IMAGE_NAME + "-cpu-" + str(os.getuid())
     build_cmd = DOCKER_TOOL + " build "
     build_cmd += "-f " + dockerfile + " "
     build_cmd += "-t " + image_name + " "
@@ -186,7 +189,7 @@ def build_with_docker(output_folder, build_types, cuda_versions, arg_strings):
     build_image_path, dockerfile = _generate_dockerfile_name(build_types, cuda_versions)
 
     if  'cuda' not in build_types or _capable_of_cuda_containers(cuda_versions):
-        image_name = build_image(build_image_path, dockerfile)
+        image_name = build_image(build_image_path, dockerfile, cuda_versions if 'cuda' in build_types else None)
     else:
         raise OpenCEError(Error.INCOMPAT_CUDA, utils.get_driver_level(), cuda_versions)
 
